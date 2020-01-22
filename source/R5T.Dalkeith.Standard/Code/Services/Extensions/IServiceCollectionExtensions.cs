35f7e48342e665a5b8ee0e56801af8cadbe158fd
MinexAutomation <minex.automation@gmail.com>
2020-01-21 16:50:05 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Dalkeith.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dalkeith.Default;
+
+
+namespace R5T.Dalkeith.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDataDirectoryNameConvention(this IServiceCollection services)
+        {
+            services.AddSingleton<IDataDirectoryNameConvention, DataDirectoryNameConvention>();
+
+            return services;
+        }
+    }
+}
